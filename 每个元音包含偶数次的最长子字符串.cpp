7c441566dc63ad3eb3528f27392707b955401bc9
JC555 <2226705824@qq.com>
2020-05-21 22:03:05 +0800

    每个元音包含偶数次的最长子字符串

---
+++ 每个元音包含偶数次的最长子字符串.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int findTheLongestSubstring(string s) {
+		int ans = 0, status = 0, n = s.length();
+		vector<int> pos(1 << 5, -1);
+		pos[0] = 0;
+		for (int i = 0; i < n; ++i) {
+			if (s[i] == 'a') {
+				status ^= 1 << 0;
+			}
+			else if (s[i] == 'e') {
+				status ^= 1 << 1;
+			}
+			else if (s[i] == 'i') {
+				status ^= 1 << 2;
+			}
+			else if (s[i] == 'o') {
+				status ^= 1 << 3;
+			}
+			else if (s[i] == 'u') {
+				status ^= 1 << 4;
+			}
+			if (~pos[status]) {
+				ans = max(ans, i + 1 - pos[status]);
+			}
+			else {
+				pos[status] = i + 1;
+			}
+		}
+		return ans;
+	}
+};
+
+int main()
+{
+	string str = "eleetminicoworoep";
+	Solution s;
+	cout << s.findTheLongestSubstring(str) << endl;
+
+
+	return 0;
+}
